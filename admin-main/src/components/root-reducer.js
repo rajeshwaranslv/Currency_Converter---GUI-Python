45c457147c14d9f0c77a758e2751e2b2d2c5b9ba
Rajeshwaran SLV <75133148+LearnTechIndia18@users.noreply.github.com>
2022-01-30 20:24:20 +0530

    Delete admin-main directory

--- admin-main/src/components/root-reducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import FAQ_Reducer from "./reducers/faqred";
-import PAGE_Reducer from "./reducers/pagered";
-const rootReducer=combineReducers({
-    FAQ_Reducer,
-    PAGE_Reducer,
-})
-export default rootReducer;
